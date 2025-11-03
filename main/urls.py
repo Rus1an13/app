c54492815b33e4bc44000c8425017c3f7c0e2f50
Rus1an13 <ruslan-1978@bk.ru>
2025-11-04 00:45:02 +0500

    second commits

---
+++ main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, URLPattern
+
+from main import views
+app_name = 'main'
+
+urlpatterns: list[URLPattern] = [
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+]
--- main/views.py
+++ main/views.py
@@ -3,13 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     context: dict[str, str] = {
-        'title': 'Home',
-        'content': 'Главная страница магазина - Home',
-        'list': ['first', 'second'],
-        'dict': {'first': 1},
-        'bool': True,
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели - Home'
     }
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('About page')
+    context: dict[str, str] = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': "Текст о том почему этот магазин такой классный, и какой хороший товар."
+    }
+    return render(request, 'main/about.html', context)
