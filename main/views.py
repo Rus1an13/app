d05512e1e8f0bdd5d4a645b57c01f7f1a4c1b6e4
Rus1an13 <ruslan-1978@bk.ru>
2025-11-03 18:25:05 +0500

    first commit

---
+++ main/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    context: dict[str, str] = {
+        'title': 'Home',
+        'content': 'Главная страница магазина - Home',
+        'list': ['first', 'second'],
+        'dict': {'first': 1},
+        'bool': True,
+    }
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    return HttpResponse('About page')
