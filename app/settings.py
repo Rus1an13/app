2304dbf485c806af48132466697719ddc8ec4c52
Ruslan <ruslan-1978@bk.ru>
2026-01-11 08:43:00 +0500

    time 9:20

--- app/settings.py
+++ app/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     'main',
     'goods',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -62,8 +63,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
