697c65509725de03556b049b007194735ecc41c0
Ruslan <ruslan-1978@bk.ru>
2026-01-12 23:17:55 +0500

    1 часть полностью

--- users/views.py
+++ users/views.py
@@ -19,6 +19,10 @@ def login(request):
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{username}, Вы вошли в аккаунт")
+
+                if request.POST.get('next', None):
+                    return HttpResponseRedirect(request.POST.get('next'))
+
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
@@ -54,6 +58,7 @@ def profile(request):
     context = {'title': 'Home - Кабинет', 'form': form}
     return render(request, 'users/profile.html', context)
 
+@login_required
 def logout(request):
     messages.success(request, f"{request.user.username}, Вы вышли из аккаунта")
     auth.logout(request)
