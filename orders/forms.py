4f0d02c6a0042f0a49ac2fd8a1e124b18cb741f8
Rus1an13 <160031151+Rus1an13@users.noreply.github.com>
2026-03-23 22:57:40 +0500

    Part 2, time 3:28

--- orders/forms.py
+++ orders/forms.py
@@ -1,4 +1,6 @@
-﻿from django import forms
+﻿import re
+
+from django import forms
 
 
 class CreateOrderForm(forms.Form):
@@ -9,6 +11,18 @@ class CreateOrderForm(forms.Form):
     requires_delivery = forms.ChoiceField(choices=[("0", "False"), ("1", "True"),],)
     delivery_address = forms.CharField(required=False)
     payment_on_get = forms.ChoiceField(choices=[("0", "False"), ("1", "True"),],)
+
+    def clean_phone_number(self):
+        data = self.cleaned_data['phone_number']
+
+        if not data.isdigit():
+            raise forms.ValidationError("Номер телефона должен содержать только цифры")
+
+        pattern = re.compile(r'^\d{10}$')
+        if not pattern.match(data):
+            raise forms.ValidationError("Неверный формат номера")
+
+        return data
 
     # first_name = forms.CharField(
     #     widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите ваше имя', }))
